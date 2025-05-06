ed82122753e535efe0117402fa337c0f5801e5c5
otnielmercea <otimercea@gmail.com>
2025-05-06 21:07:14 +0000

    Changes

--- _plugins/google-scholar-citations.rb
+++ _plugins/google-scholar-citations.rb
@@ -37,7 +37,7 @@ module Jekyll
           end
 
           # Sleep for a random amount of time to avoid being blocked
-          sleep(rand(45..65))
+          sleep(rand(15..35))
 
           # Fetch the article page
           doc = Nokogiri::HTML(URI.open(article_url, "User-Agent" => "Ruby/#{RUBY_VERSION}"))
